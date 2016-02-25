e8c2f9452615ead22388e0d0c268a907da2eb146
Mathieu Delacroix <dlacreme@gmail.com>
2016-02-25 12:07:21 +0100

    update du playground

--- app/people/people.service.js
+++ app/people/people.service.js
@@ -28,31 +28,49 @@
         this.remove = function (id) {
             $http.delete('http://mon-api.com/dude/' + id);
         };
-    })
-    .factory('dudeFactory', function ($http) {
-        return {
-            getAll: function () {
-                return  [
-                    {
-                        id: 1,
-                        firstname: 'John',
-                        lastname: 'Coffey',
-                        age: 43
-                    },
-                    {
-                        id: 2,
-                        firstname: 'John',
-                        lastname: 'Cena',
-                        age: 38
-                    },
-                    {
-                        id: 3,
-                        firstname: 'John',
-                        lastname: 'Travolta',
-                        age: 62
-                    }
-                ];
-            }
-        };
     });
+
+
+    angular.module('demo.people')
+        .factory('Dude', factoryDude);
+
+    factoryDude.$inject = [
+        '$http'
+    ];
+
+    function factoryDude($http) {
+
+        function Dude(id, firstname, lastname, age) {
+            this.id = id;
+            this.firstname = firsntame;
+            this.lastname = lastname;
+            this.age = age;
+
+            this.getId = function() {
+                return this.id;
+            };
+
+            this.printInfo = function() {
+                console.log('Hi! I\'am ' + this.firstname + 'and I\'am ' + this.age);
+            };
+
+            this.getInfo = function() {
+                $http.get('myurl.com/dude' + this.id).then(function(dude) {
+                    console.log('Dude get > ', dude);
+                });
+            };
+
+            this.setInfo = function() {
+                $http.post('myurl.com/dude/' + this.id, {
+                    firstname: firstname
+                    lastname: lastname,
+                    age: age
+                }).then(function() {
+                    console.log('New dude !!');
+                });
+            };
+        }
+        return Dude;
+    }
+
 })();
